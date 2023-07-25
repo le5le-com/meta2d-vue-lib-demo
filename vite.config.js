61b19d13d15aed05575c5f9777ef99a4b2228ba5
ananzhusen <2459622492@qq.com>
2023-07-25 10:09:50 +0800

    feat_vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,58 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs';
+import formidable from 'formidable';
+import { resolve, join } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    fileList(),
+  ]
 })
+
+function fileList() {
+  return {
+    name: 'vite-plugin-svg-png-files',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        const url = req.url ;
+
+        if (
+          (url.startsWith('/svg/') || url.startsWith('/png/')) &&
+          url.endsWith('/')
+        ) {
+          const pwd = decodeURI(join(__dirname, 'public', url));
+          const files = fs.readdirSync(pwd, {
+            withFileTypes: true,
+          });
+
+          const list= [];
+          for (const item of files) {
+            if (item.isDirectory()) {
+              list.push({ name: item.name, type: 'directory' });
+            } else {
+              list.push({ name: item.name });
+            }
+          }
+          res.end(JSON.stringify(list));
+        } else if (url === '/img' && req.method === 'POST') {
+          const form = formidable({
+            uploadDir: decodeURI(join(__dirname, 'public', '/img')),
+            keepExtensions: true,
+          });
+          form.parse(req, (err, fields, files) => {
+            if (!err) {
+              res.end(
+                JSON.stringify({ url: '/img/' + files.file.newFilename })
+              );
+            }
+          });
+        } else {
+          next();
+        }
+      });
+    },
+  };
+}
