bafe5e20ffac18d391cd5c2c47fb941b8cadb458
JosephHo <32008dianchenan@163.com>
2023-11-03 18:32:21 +0800

    fix:test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/index';
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router/index"
 
-import store from './store';
+import store from "./store"
 
-import meta2d from 'meta2d-vue/meta2d-vue.es.js';
+import meta2d from "meta2d-vue/meta2d-vue.es.js"
 
-import 'meta2d-vue/style.css';
+import "meta2d-vue/style.css"
 
-createApp(App).use(router).use(store).use(meta2d,{store}).mount('#app');
+createApp(App).use(router).use(store).use(meta2d, { store }).mount("#app")
